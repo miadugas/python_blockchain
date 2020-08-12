e6215f4a1ffec5af7ea55660623d1375d4339628
Mia Dugas <miadugas@outlook.com>
2020-08-12 14:44:48 -0600

    updated readme

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,14 @@
-blockchain = [1]
+blockchain = []
 
-def add_value():
-    blockchain.append([blockchain[0],5.3])
-    print(blockchain)
+def get_last_blockchain_value():
+    return blockchain[-1]
 
-add_value()
-add_value()
-add_value()
+def add_value(transaction_amount,last_transaction=[1]):
+    blockchain.append([last_transaction,transaction_amount])
+
+
+add_value(2)
+add_value(last_transaction=get_last_blockchain_value(), transaction_amount=0.9)
+add_value(10.89, get_last_blockchain_value())
+
+print(blockchain)
