19f14c4714600a9b1570fdab022057a0135a331b
Mia Dugas <miadugas@outlook.com>
2020-08-12 19:26:59 -0600

    cleaning up blockchain

--- blockchain.py
+++ blockchain.py
@@ -16,23 +16,40 @@ def add_value(transaction_amount,last_transaction=[1]):
     """
     blockchain.append([last_transaction,transaction_amount])
 
-def get_user_input():
+
+def get_transaction_value():
     """ Returns the input of the user(a new transaction amount) as a float"""
+    # Get the user input, transform it from a string to a flost and store it
     user_input = float(input('Your transaction amount please: '))
     return user_input
 
-# Get the first transaction input and add the value to the blockchain
-tx_amount=get_user_input()
-add_value(tx_amount)
 
-# Get the second & third transaction input and add the value to the blockchain
-while True:
-    tx_amount=get_user_input()
-    add_value(tx_amount, get_last_blockchain_value())
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
 
+
+def print_blockchain_elements():        
     # Output the blockchain list to the console
     for block in blockchain:
         print('Outputting Block')
         print(block)
+
+# Get the first transaction input and add the value to the blockchain
+tx_amount = get_transaction_value()
+add_value(tx_amount)
+
+# Get the second & third transaction input and add the value to the blockchain
+while True:
+    print('Please choose an option')
+    print('1: Add a new transaction value')
+    print('2: Output the block chain blocks')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain_value())
+    else:
+        print_blockchain_elements()
+
 
 print('Done!')
