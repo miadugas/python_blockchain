be87471ab237dfe20880644db4e407d6fbb17ea3
Mia Dugas <miadugas@outlook.com>
2020-08-15 20:02:55 -0600

    added more logic

--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,6 @@
 # Initializing our (empty) blockchain list
+MINING_REWARD = 10
+
 genesis_block = {
         'previous_hash':'', 
         'index': 0, 
@@ -7,10 +9,31 @@ genesis_block = {
 blockchain = [genesis_block]
 open_transactions = []
 owner = 'Mia'
+participants = {'Mia'}
 
 
 def hash_block(block):
     return '-'.join([str(block[key]) for key in block])
+
+
+# logic to calculate if participents have the currency to complete transactions
+def get_balance(participant):
+    tx_sender = [[tx['amount'] for tx in block['transactions'] if tx ['sender'] == participant] for block in blockchain]
+    # dont allow overdraft for chained transactions & calculate balance better
+    open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
+    tx_sender.append(open_tx_sender)
+    amount_sent = 0
+    # get tx amount
+    for tx in tx_sender:
+        if len(tx) > 0:
+            amount_sent += tx[0]
+    tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx ['recipient'] == participant] for block in blockchain]
+    amount_received = 0
+    # get tx balance
+    for tx in tx_recipient:
+        if len(tx) > 0:
+            amount_received += tx[0]
+    return amount_received - amount_sent
 
 
 def get_last_blockchain_value():
@@ -18,6 +41,13 @@ def get_last_blockchain_value():
     if len(blockchain) < 1:
         return None
     return blockchain[-1]
+
+
+# verify if sender has the blance to send a tx
+def get_transaction(transaction):
+    sender_balance = get_balance(transaction['sender'])
+    return sender_balance >= transaction['amount']
+        
 
 # This function accepts the arguments
 # One required one (transaction_amount) and one optional one (last_transaction)
@@ -36,19 +66,33 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         'sender': sender, 
         'recipient': recipient, 
         'amount': amount
-        }
-    open_transactions.append(transaction)
-    
+    }
+    if verify_transaction(transaction):
+        open_transactions.append(transaction)
+        participants.add(sender)
+        participants.add(recipient)
+        return True
+    return False
 
 def mine_block():
     last_block = blockchain[-1]
     hashed_block = hash_block(last_block)
+    # reward for mining blocks
+    reward_transaction = {
+        'sender': 'MINING',
+        'recipient': owner,
+        'amount': MINING_REWARD
+    }
+    copied_transactions = open_transactions
+    open_transactions.append(reward_transaction)
     block = {
         'previous_hash':hashed_block, 
         'index': len(blockchain), 
         'transactions': open_transactions
     }
     blockchain.append(block)
+    #reset open transactions
+    return True
 
 
 def get_transaction_value():
@@ -114,6 +158,7 @@ while waiting_for_input:
     print('1: Add a new transaction value')
     print('2: Mine a new block')
     print('3: Output the blockchain blocks')
+    print('4: Output participents')
     print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
@@ -122,12 +167,18 @@ while waiting_for_input:
         # grab tuple data
         recipient, amount = tx_data
         # Add the transaction amount to the blockchain
-        add_transaction(recipient, amount=amount)
+        if add_transaction(recipient, amount=amount):
+            print('Added Transaction!')
+        else:
+            print('Transaction Failed! Please verify your balance before resending')
         print(open_transactions)
     elif user_choice == '2':
-        mine_block()
+        if mine_block():
+            open_transactions = []
     elif user_choice == '3':
         print_blockchain_elements()
+    elif user_choice == '4':
+        print(participants)
     elif user_choice == 'h':
         # Make sure that no one tries to "hack" the blockchain if its empty
         if len(blockchain) >=1:
@@ -146,6 +197,7 @@ while waiting_for_input:
         print('Invalid blockchain!')
         # Break out of the loop
         break
+    print(get_balance('Mia'))
 else:
     print('User has left!')
 
