dd2843332caf9162c688768a4d6878efb48b8b30
Mia Dugas <miadugas@outlook.com>
2020-08-17 19:00:45 -0600

    reducing lists

--- blockchain.py
+++ blockchain.py
@@ -1,38 +1,54 @@
+import functools
 # Initializing our (empty) blockchain list
 MINING_REWARD = 10
 
+# The starting block for the blockchain
 genesis_block = {
         'previous_hash':'', 
         'index': 0, 
         'transactions': []
 }
+# Initializing the (empty) blockchain list
 blockchain = [genesis_block]
+# Unhandled transactions
 open_transactions = []
+# Im the the owner of this blockchain node, hence this is our identifier (e.g. for sending coins)
 owner = 'Mia'
+# Registered participants: Ourself + other people sending/ receiving coins
 participants = {'Mia'}
 
 
+
 def hash_block(block):
+    """Hashes a block and returns a string representation of it.
+    
+    Arguments:
+        :block: The block that should be hashed.
+    """
     return '-'.join([str(block[key]) for key in block])
 
 
 # logic to calculate if participents have the currency to complete transactions
 def get_balance(participant):
-    tx_sender = [[tx['amount'] for tx in block['transactions'] if tx ['sender'] == participant] for block in blockchain]
+    """Calculate and return the balance for a participant.
+
+    Arguments:
+        :participant: The person for whom to calculate the balance.
+    """
+    # Fetch a list of all sent coin amounts for the given person (empty lists are returned if the person was NOT the sender)
     # dont allow overdraft for chained transactions & calculate balance better
+    tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
+    # Fetch a list of all sent coin amounts for the given person (empty lists are returned if the person was NOT the sender)
+    # This fetches sent amounts of open transactions (to avoid double spending)
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    # get tx amount
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
-    tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx ['recipient'] == participant] for block in blockchain]
-    amount_received = 0
-    # get tx balance
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
+    amount_sent = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)
+    # This fetches received coin amounts of transactions that were already included in blocks of the blockchain
+    # We ignore open transactions here because you shouldn't be able to spend coins before the transaction was confirmed + included in a block
+
+    tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant] for block in blockchain]
+    amount_received = functools.reduce(lambda tx_sum, tx_amt: tx_sum + tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)
+    # Return the total balance
     return amount_received - amount_sent
 
 
@@ -75,14 +91,19 @@ def add_transaction(recipient, sender=owner, amount=1.0):
     return False
 
 def mine_block():
+    """Create a new block and add open transactions to it."""
+    # Fetch the currently last block of the blockchain
     last_block = blockchain[-1]
+    # Hash the last block (=> to be able to compare it to the stored hash value)
     hashed_block = hash_block(last_block)
-    # reward for mining blocks
+    # reward to miners for mining blocks
     reward_transaction = {
         'sender': 'MINING',
         'recipient': owner,
         'amount': MINING_REWARD
     }
+    # Copy transaction instead of manipulating the original open_transactions list
+    # This ensures that if for some reason the mining should fail, we don't have the reward transaction stored in the open transactions
     copied_transactions = open_transactions[:]
     copied_transactions.append(reward_transaction)
     block = {
@@ -104,6 +125,7 @@ def get_transaction_value():
 
 
 def get_user_choice():
+    """Prompts the user for a choice and return it."""
     user_input = input('Your choice: ')
     return user_input
 
